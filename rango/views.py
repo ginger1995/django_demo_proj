9134c4861a17811199ca813d4577d647725f1c48
ginger1995 <jbw7@qq.com>
2016-12-15 17:12:56 +0800

    .

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,'pages':page_list}
     return render(request, 'rango/index.html', context_dict)
 
 
@@ -24,3 +25,27 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
         context_dict['category'] = None
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        # print request.POST  
+        form = CategoryForm(request.POST)
+        if form.is_valid:
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    return render(request, 'rango/add_category.html', {'form': form})
+    
+
+
+
+
+
+
+
+
+
+
