4eeba125d038757776c39752cf5ca462fd78c071
ginger1995 <jbw7@qq.com>
2016-12-19 17:08:03 +0800

    .

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
 # Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -35,4 +36,4 @@ class Page(models.Model):
         return self.title
 
     def __unicode__(self):
-        return self.name
+        return self.title
