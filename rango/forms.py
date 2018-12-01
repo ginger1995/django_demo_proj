4f3bd673ccdc4c1e91f8c53a4a659c822826dae6
ginger1995 <jbw7@qq.com>
2018-12-01 22:51:44 +0800

    20181201

--- rango/forms.py
+++ rango/forms.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # coding=utf-8
 from django import forms
-from rango.models import Category,Page
+from rango.models import Category, Page
+
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     #views = forms.IntegerField(help_text="Please enter the views.")
-    likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
-    slug = forms.CharField(widget=forms.HiddenInput(),required=False)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Category
         fields = ('name',)
 
+
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,help_text="Please enter the url of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the url of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     def clean(self):
         cleaned_data = self.cleaned_data
